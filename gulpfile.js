bdd727dc5ab98d17fc5067e8ceb12a143dbf8d07
david-nicholson <djn000@gmail.com>
2015-09-17 11:46:16 +0100

    Initial skeleton

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./src/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./demo/styles'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/**/*.scss', ['sass']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'demo',
+    livereload: true
+  });
+});
+
+gulp.task('default', [
+  'connect',
+  'sass:watch'
+]);
